25b1f77d320c53d70a6be564f80189e115e1ad70
Alireza <josheghani.dev@gmail.com>
2017-01-29 19:48:45 +0330

    Moved faker base method to faker object

--- src/Faker.php
+++ src/Faker.php
@@ -17,7 +17,39 @@ namespace Josh\Faker;
  * @property string $website
  * @property string $pageUrl
  */
-class Faker extends FakerBase {
+class Faker
+{
+
+    /**
+     * Getter gender type
+     *
+     * @var string | null
+     */
+    protected $gender = null;
+
+    /**
+     * Set male format of firstnames
+     *
+     * @return $this
+     */
+    public function male()
+    {
+        $this->gender = 'male';
+
+        return $this;
+    }
+
+    /**
+     * Set female format of firstnames
+     *
+     * @return $this
+     */
+    public function female()
+    {
+        $this->gender = 'female';
+
+        return $this;
+    }
 
     /**
      * Call method from magic method
