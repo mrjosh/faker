d94c26b387cbe74dc229f4a61e9aa9debaa01d7d
Alireza Josheghani <a.josheghani@anetwork.ir>
2017-02-04 12:24:00 +0330

    Update unit tests configures Added php5.6 and hhvm to travis-ci file Update version of phpunit version

--- tests/FakerTest.php
+++ tests/FakerTest.php
@@ -1,11 +1,17 @@
 <?php
 
 use Josh\Faker\Faker;
+use function Josh\Faker\isMeliCode;
 
 class FakerTest extends PHPUnit_Framework_TestCase
 {
     protected $objects = [];
 
+    /**
+     * FakerTest constructor.
+     *
+     * @return void
+     */
     public function __construct()
     {
         parent::__construct();
@@ -13,21 +19,49 @@ class FakerTest extends PHPUnit_Framework_TestCase
         $this->objects = require __DIR__ . '/../objects.php';
     }
 
+    /**
+     * Test firstname genrator
+     *
+     * @author Alireza Josheghani <josheghani.dev@gmail.com>
+     * @since 4 Feb 2017
+     * @return void
+     */
     public function testFirstName()
     {
         $this->assertTrue(in_array(Faker::firstname(),$this->objects['names']));
     }
 
+    /**
+     * Test lastname genrator
+     *
+     * @author Alireza Josheghani <josheghani.dev@gmail.com>
+     * @since 4 Feb 2017
+     * @return void
+     */
     public function testLastName()
     {
         $this->assertTrue(in_array(Faker::lastname(),$this->objects['families']));
     }
 
+    /**
+     * Test company genrator
+     *
+     * @author Alireza Josheghani <josheghani.dev@gmail.com>
+     * @since 4 Feb 2017
+     * @return void
+     */
     public function testCompany()
     {
         $this->assertTrue(in_array(Faker::company(),$this->objects['companies']));
     }
 
+    /**
+     * Test mobile genrator
+     *
+     * @author Alireza Josheghani <josheghani.dev@gmail.com>
+     * @since 4 Feb 2017
+     * @return void
+     */
     public function testMobile()
     {
         $phonePrefix = substr(str_replace(0,'',Faker::mobile()), 0, 3);
@@ -35,6 +69,13 @@ class FakerTest extends PHPUnit_Framework_TestCase
         $this->assertTrue(in_array($phonePrefix,$this->objects['prefixTelePhones']));
     }
 
+    /**
+     * Test phone genrator
+     *
+     * @author Alireza Josheghani <josheghani.dev@gmail.com>
+     * @since 4 Feb 2017
+     * @return void
+     */
     public function testPhone()
     {
         $all = $this->objects['prefixPhones'];
@@ -61,9 +102,16 @@ class FakerTest extends PHPUnit_Framework_TestCase
         $this->assertNotFalse($test);
     }
 
+    /**
+     * Test age genrator
+     *
+     * @author Alireza Josheghani <josheghani.dev@gmail.com>
+     * @since 4 Feb 2017
+     * @return void
+     */
     public function testAge()
     {
-        $age = \Josh\Faker\faker()->age;
+        $age = Faker::age();
 
         $assert = false;
 
@@ -76,26 +124,54 @@ class FakerTest extends PHPUnit_Framework_TestCase
         $this->assertNotFalse($assert);
     }
 
+    /**
+     * Test email genrator
+     *
+     * @author Alireza Josheghani <josheghani.dev@gmail.com>
+     * @since 4 Feb 2017
+     * @return void
+     */
     public function testEmail()
     {
-        $email = \Josh\Faker\faker()->email;
+        $email = Faker::email();
 
         $this->assertNotFalse(filter_var($email, FILTER_VALIDATE_EMAIL));
     }
 
+    /**
+     * Test domain genrator
+     *
+     * @author Alireza Josheghani <josheghani.dev@gmail.com>
+     * @since 4 Feb 2017
+     * @return void
+     */
     public function testDomain()
     {
         $this->assertRegExp('/^(?:[-A-Za-z0-9]+\.)+[A-Za-z]{2,6}$/',Faker::domain());
     }
 
+    /**
+     * Test city genrator
+     *
+     * @author Vahid Almasi <vahid.almasi71@gmail.com>
+     * @since 4 Feb 2017
+     * @return void
+     */
     public function testCity()
     {
         $this->assertTrue(in_array(Faker::city(),$this->objects['city']));
     }
 
+    /**
+     * Test melicode genrator
+     *
+     * @author Alireza Josheghani <josheghani.dev@gmail.com>
+     * @since 4 Feb 2017
+     * @return void
+     */
     public function testMeliCode()
     {
-        $this->assertTrue(\Josh\Faker\isMeliCode(Faker::meliCode()));
+        $this->assertTrue(isMeliCode(Faker::meliCode()));
     }
     
 }
